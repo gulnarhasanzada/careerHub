d5593f51deef70a61450b556dc53154890e7b976
Gulnar <gulnar.hesenzade@gmail.com>
2025-08-04 14:23:29 -0400

    Remove dist folders from tracking and add gitignore

--- shared/utils/dist/constants.d.ts
+++
@@ -1,8 +0,0 @@
-export declare const HTTP_STATUS: {
-    readonly OK: 200;
-    readonly CREATED: 201;
-    readonly BAD_REQUEST: 400;
-    readonly UNAUTHORIZED: 401;
-    readonly NOT_FOUND: 404;
-    readonly SERVER_ERROR: 500;
-};
--- shared/utils/dist/formatting.d.ts
+++
@@ -1,2 +0,0 @@
-export declare const formatDate: (date: Date) => string;
-export declare const truncateText: (text: string, maxLength: number) => string;
